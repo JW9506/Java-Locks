313c5754d9b7743d4be766d5b13eba35c2f6e17a
JW9506 <cf.dota@gmail.com>
2023-04-14 02:53:50 -0500

    Save

--- src/main/java/com/quickstart/_threadcommunication/Main.java
+++ src/main/java/com/quickstart/_threadcommunication/Main.java
@@ -8,8 +8,34 @@ import lombok.NoArgsConstructor;
 public class Main {
   public static void main(String[] args) {
     Account acc = new Account("1234567", new BigDecimal("10000"));
-    new DrawThread("甲", acc, new BigDecimal("8000")).start();
-    new DrawThread("乙", acc, new BigDecimal("8000")).start();
+    new DrawThread("甲", acc, new BigDecimal("10000")).start();
+    new DrawThread("乙", acc, new BigDecimal("10000")).start();
+    new SaveThread("父1", acc, new BigDecimal("10000")).start();
+    new SaveThread("父2", acc, new BigDecimal("10000")).start();
+    new SaveThread("父3", acc, new BigDecimal("10000")).start();
+  }
+}
+
+class SaveThread extends Thread {
+
+  private Account account;
+  private BigDecimal drawAmount;
+
+  public SaveThread(String name, Account account, BigDecimal drawAmount) {
+    super(name);
+    this.account = account;
+    this.drawAmount = drawAmount;
+  }
+
+  @Override
+  public void run() {
+    try {
+      while (true) {
+        account.save(drawAmount);
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
 
@@ -26,7 +52,13 @@ class DrawThread extends Thread {
 
   @Override
   public void run() {
-    account.draw(drawAmount);
+    try {
+      while (true) {
+        account.draw(drawAmount);
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
 
@@ -38,25 +70,33 @@ class Account {
   private String accountNo;
   private BigDecimal bal;
 
-  public synchronized void draw(BigDecimal drawAmount) {
+  public synchronized void draw(BigDecimal drawAmount) throws InterruptedException {
     if (bal.compareTo(drawAmount) >= 0) {
-      System.out.println(Thread.currentThread().getName() + "取钱成功！吐出钞票：" + drawAmount);
-      try {
-        Thread.sleep(1);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
       bal = bal.subtract(drawAmount);
+      Thread.sleep(1000);
+      System.out.println(Thread.currentThread().getName() + "取钱成功！吐出钞票：" + drawAmount);
       this.notifyAll();
-      try {
-        this.wait();
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
+      this.wait();
       System.out.println("\t余额为：" + bal);
     } else {
+      this.notifyAll();
+      this.wait();
       System.out.println(Thread.currentThread().getName() + "取钱失败！余额不足！");
     }
-    this.notifyAll();
+  }
+
+  public synchronized void save(BigDecimal saveAmount) throws InterruptedException {
+    if (bal.equals(BigDecimal.valueOf(0))) {
+      bal = bal.add(saveAmount);
+      Thread.sleep(1000);
+      System.out.println(Thread.currentThread().getName() + "存钱成功：" + saveAmount);
+      this.notifyAll();
+      this.wait();
+      System.out.println("\t余额为：" + bal);
+    } else {
+      this.notifyAll();
+      this.wait();
+      System.out.println(Thread.currentThread().getName() + "发现已有存款，无需存钱！");
+    }
   }
 }

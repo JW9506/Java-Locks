cdd4315ae4420e6deb2df5ab272b9a032751c227
JW9506 <cf.dota@gmail.com>
2023-04-17 04:52:22 -0500

    Save

---
+++ src/main/java/com/quickstart/_volatile/Main.java
@@ -0,0 +1,38 @@
+package com.quickstart._volatile;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+  public static void main(String[] args) {
+    MyRunnable r = new MyRunnable();
+    new Thread(r).start();
+    while (true) {
+      if (r.isFlag()) {
+        log.info("main thread is over");
+        break;
+      }
+    }
+  }
+}
+
+@Slf4j
+@Data
+class MyRunnable implements Runnable {
+  
+  // volatile private boolean flag = false;
+  private boolean flag = false;
+
+  @Override
+  public void run() {
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    
+    setFlag(true);
+    log.info("flag: {}", flag);
+  }
+}

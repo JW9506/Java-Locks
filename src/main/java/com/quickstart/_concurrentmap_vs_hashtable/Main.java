ad17efeeaf28becabf5c3fbdab8302034c1cb37e
JW9506 <cf.dota@gmail.com>
2023-12-28 18:11:31 -0600

    Add comments

--- src/main/java/com/quickstart/_concurrentmap_vs_hashtable/Main.java
+++ src/main/java/com/quickstart/_concurrentmap_vs_hashtable/Main.java
@@ -8,23 +8,30 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
+
+  // Using HashMap for thread unsafety, for demo purposes.
+  // public static Map<String, String> map = new HashMap<>();
+
+  // Using HashTable for thread safety with mediocre performance.
+  // public static Map<String, String> map = new Hashtable<>();
+
+  // Using ConcurrentHashMap for thread safety with better performance.
+  public static Map<String, String> map = new ConcurrentHashMap<>();
+
   public static void main(String[] args) throws InterruptedException {
     Thread t1 = new Thread(new MyRunnable(), "T1");
     Thread t2 = new Thread(new MyRunnable(), "T2");
     t1.start();
     t2.start();
+
+    // Join to ensure both threads complete before calculating the map size
     t1.join();
     t2.join();
+
     log.info("Map size: {}", map.size());
   }
-
-  // not Thread safe, but fast.
-  // public static Map<String, String> map = new HashMap<>();
-  // Thread safe, but slow. All instance methods are synchronized.
-  // public static Map<String, String> map = new Hashtable<>();
-  // Thread safe. Compare and swap, locally synchronized.
-  public static Map<String, String> map = new ConcurrentHashMap<>();
 }
+
 
 @Slf4j
 class MyRunnable implements Runnable {
@@ -32,10 +39,13 @@ class MyRunnable implements Runnable {
   @Override
   public void run() {
     long begin = System.currentTimeMillis();
+
+    // Using thread name and loop index as the key to avoid overwriting in the map
     for (int i = 0; i < 500000; i++) {
-      Main.map.put(Thread.currentThread().getName() + i, "" + i);
+      Main.map.put(Thread.currentThread().getName() + "-" + i, Integer.toString(i));
     }
+
     long end = System.currentTimeMillis();
-    log.info("Time spent: {}", (end - begin));
+    log.info("Time spent by {}: {} ms", Thread.currentThread().getName(), (end - begin));
   }
 }

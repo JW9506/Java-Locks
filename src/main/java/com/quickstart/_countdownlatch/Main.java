aca2610c0ed01dedd5f31155da785049a108d7ce
JW9506 <cf.dota@gmail.com>
2023-12-28 18:29:29 -0600

    Thread A and B share a common CountDownLatch

--- src/main/java/com/quickstart/_countdownlatch/Main.java
+++ src/main/java/com/quickstart/_countdownlatch/Main.java
@@ -1,23 +1,32 @@
 package com.quickstart._countdownlatch;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * CountDownLatch allows one or more threads to wait until other threads are finished, to get into Running state.
  * 
  * Given:
  *    Thread A is going to print A and C
  *    Thread B is going to print B
- * Make sure A, B, C are logged.
+ * Make sure A, B, C are logged in this order.
  * 
  */
 
 public class Main {
   public static void main(String[] args) {
-    new ThreadA().start();
-    new ThreadB().start();
+    CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    new ThreadA(countDownLatch).start();
+    new ThreadB(countDownLatch).start();
   }
 }
 
 class ThreadA extends Thread {
+
+  private CountDownLatch countDownLatch;
+  public ThreadA(CountDownLatch countDownLatch) {
+    this.countDownLatch = countDownLatch;
+  }
 
   @Override
   public void run() {
@@ -27,6 +36,11 @@ class ThreadA extends Thread {
 }
 
 class ThreadB extends Thread {
+
+  private CountDownLatch countDownLatch;
+  public ThreadB(CountDownLatch countDownLatch) {
+    this.countDownLatch = countDownLatch;
+  }
 
   @Override
   public void run() {
